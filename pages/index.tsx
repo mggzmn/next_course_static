ebc412e69c6cdf3779633f7bbe358fe03cd62cb4
Mariana Guzmán <mggzmn91@gmail.com>
2022-04-05 10:21:59 -0500

    NextUI pokemon card

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,35 @@
-import { Button } from '@nextui-org/react';
 import type { NextPage, GetStaticProps } from 'next';
+import { Card, Grid, Row, Text } from '@nextui-org/react';
+
 import Head from 'next/head';
 import { pokeApi } from '../api';
 import { Layout } from '../components/layouts';
-import { PokemonListResponse } from '../interfaces';
-const HomePage: NextPage = (props) => {
-  console.log(props);
+import { PokemonListResponse, SmallPokemon } from '../interfaces';
+
+interface Props {
+  pokemons: SmallPokemon[];
+}
+
+const HomePage: NextPage<Props> = ({ pokemons }) => {
   return (
     <Layout title='Listado de Pókemons'>
-      <ul>
-        <li></li>
-      </ul>
+      <Grid.Container gap={2} justify='flex-start'>
+        {pokemons.map(({ id, name, img }) => (
+          <Grid xs={6} sm={3} md={2} xl={1} key={id}>
+            <Card hoverable clickable>
+              <Card.Body css={{ p: 1 }}>
+                <Card.Image src={img} width='100%' height={140} />
+              </Card.Body>
+              <Card.Footer>
+                <Row justify='space-between'>
+                  <Text transform='capitalize'>{name}</Text>
+                  <Text>#{id}</Text>
+                </Row>
+              </Card.Footer>
+            </Card>
+          </Grid>
+        ))}
+      </Grid.Container>
     </Layout>
   );
 };
@@ -24,9 +43,15 @@ const HomePage: NextPage = (props) => {
 export const getStaticProps: GetStaticProps = async (ctx) => {
   const { data } = await pokeApi.get<PokemonListResponse>('/pokemon?limit=151');
 
+  const pokemons: SmallPokemon[] = data.results.map((poke, i) => ({
+    ...poke,
+    id: i + 1,
+    img: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${i + 1}.svg`,
+  }));
+
   return {
     props: {
-      pokemons: data.results,
+      pokemons,
     },
   };
 };
