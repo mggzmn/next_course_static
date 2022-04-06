e5a598256f48123bcbddccdcc60ef3e49140bc09
Mariana Guzmán <mggzmn91@gmail.com>
2022-04-06 10:39:06 -0500

    Pokemon Card

--- pages/pokemon/[id].tsx
+++ pages/pokemon/[id].tsx
@@ -1,5 +1,5 @@
+import { Button, Card, Container, Grid, Text, Image } from '@nextui-org/react';
 import { GetStaticProps, GetStaticPaths, NextPage } from 'next';
-import { useRouter } from 'next/router';
 import { pokeApi } from '../../api';
 import { Layout } from '../../components/layouts';
 import { Pokemon } from '../../interfaces';
@@ -11,14 +11,42 @@ const PokemonPage: NextPage<Props> = ({ pokemon }) => {
   /* const router = useRouter(); */
   return (
     <Layout title='algun poke'>
-      <h1>{pokemon.name}</h1>
+      <Grid.Container css={{ marginTop: '5px' }} gap={2}>
+        <Grid xs={12} sm={4}>
+          <Card hoverable css={{ padding: '30px' }}>
+            <Card.Body>
+              <Card.Image src={pokemon.sprites.other?.dream_world.front_default || '/no-image.png'} alt={pokemon.name} width='100%' height={200} />
+            </Card.Body>
+          </Card>
+        </Grid>
+        <Grid xs={12} sm={8}>
+          <Card>
+            <Card.Header css={{ display: 'flex', justifyContent: 'space-between' }}>
+              <Text h1 transform='capitalize'>
+                {pokemon.name}
+              </Text>
+              <Button color='gradient' ghost>
+                Guardar en Favoritos
+              </Button>
+            </Card.Header>
+            <Card.Body>
+              <Text size={30}>Sprites</Text>
+              <Container display='flex' direction='row'>
+                <Image src={pokemon.sprites.front_default} alt={pokemon.name} width={100} height={100} />
+                <Image src={pokemon.sprites.back_default} alt={pokemon.name} width={100} height={100} />
+                <Image src={pokemon.sprites.front_shiny} alt={pokemon.name} width={100} height={100} />
+                <Image src={pokemon.sprites.back_shiny} alt={pokemon.name} width={100} height={100} />
+             </Container>
+            </Card.Body>
+          </Card>
+        </Grid>
+      </Grid.Container>
     </Layout>
   );
 };
 // You should use getStaticPaths if you’re statically pre-rendering pages that use dynamic routes
 
 export const getStaticPaths: GetStaticPaths = async (ctx) => {
-  // const { data } = await  // your fetch function here
   const pokemon151 = [...Array(151)].map((value, index) => `${index + 1}`);
   return {
     paths: pokemon151.map((id) => ({
