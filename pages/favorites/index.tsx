6a2d3718f419a5287664e769c44e6e2959ad473d
Mariana Guzmán <mggzmn91@gmail.com>
2022-04-08 11:00:00 -0500

    favorite list

--- pages/favorites/index.tsx
+++ pages/favorites/index.tsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from 'react';
+import { Card, Grid } from '@nextui-org/react';
 import { Layout } from '../../components/layouts';
 import { NoFavorites } from '../../components/ui';
 import { localFavorites } from '../../utils';
@@ -6,12 +7,28 @@ import { localFavorites } from '../../utils';
 const FavoritesPage = () => {
   const [favoritePokemons, setFavoritePokemons] = useState<number[]>([]);
   useEffect(() => {
-    setFavoritePokemons(localFavorites.pokemons)
-  }, [])
-  
+    setFavoritePokemons(localFavorites.pokemons);
+  }, []);
+
   return (
     <Layout title='Favoritos'>
-      <NoFavorites />
+      {favoritePokemons.length === 0 ? (
+        <NoFavorites />
+      ) : (
+        <Grid.Container gap={2} direction='row' justify='flex-start'>
+          {favoritePokemons.map((id) => (
+            <Grid xs={6} sm={3} md={2} key={id}>
+              <Card hoverable clickable css={{ padding: 10 }}>
+                <Card.Image
+                  src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${id}.svg`}
+                  width={'100%'}
+                  height={140}
+                />
+              </Card>
+            </Grid>
+          ))}
+        </Grid.Container>
+      )}
     </Layout>
   );
 };
