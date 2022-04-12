629f9f11805dc422051920beb3486ed977e13f30
Mariana Guzmán <mggzmn91@gmail.com>
2022-04-12 12:01:22 -0500

    Open graph meta tags

--- components/layouts/Layout.tsx
+++ components/layouts/Layout.tsx
@@ -6,6 +6,7 @@ import { Navbar } from '../ui';
 interface Props {
   title?: string;
 }
+const origin = typeof window === 'undefined' ? '' : window.location.origin;
 
 export const Layout: FC<Props> = ({ children, title }) => {
   return (
@@ -15,6 +16,10 @@ export const Layout: FC<Props> = ({ children, title }) => {
         <meta name='author' content='Mariana Guzmán' />
         <meta name='description' content={`Información sobre el pókemon ${title}`} />
         <meta name='keywords' content={`${title}, pokemon, pokedex`} />
+
+        <meta property='og:title' content={`Información sobre ${title}`} />
+        <meta property='og:description' content={`Esta página habla sobre ${title} `} />
+        <meta property='og:image' content={`${origin}/img/banner.png`} />
       </Head>
       <Navbar />
       <main style={{ padding: '0px 20px' }}>{children}</main>
